4da72c69166b6b9c34b7cb966d7857e2168549f9
NIKHIL NAGAR <nagarn2005@gmail.com>
2024-07-25 06:58:13 +0000

    [LeetCode Sync] Runtime - 11 ms (7.56%), Memory - 21.9 MB (100.00%)

--- solutions/0283-move-zeroes/solution.cpp
+++ solutions/0283-move-zeroes/solution.cpp
@@ -2,17 +2,15 @@ class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
       int n=nums.size();
-      int j=0;
-      for(int i=0;i<n;i++){
-if(nums[i] !=0){
-nums[j] = nums[i];
-j++;   
-}
-      } 
-while(j < n){
-nums[j]=0;
-j++;
-}
+int i=0;
 
+for(int j=0;j<n;j++){
+
+  if(nums[j] != 0){
+swap(nums[i],nums[j]);
+i++;
+  }
+
+}
     }
 };
