9c4abbad29154775d520bd27f41d2c26355887a2
NIKHIL NAGAR <nagarn2005@gmail.com>
2024-07-29 16:02:35 +0000

    [LeetCode Sync] Runtime - 66 ms (15.73%), Memory - 27.5 MB (99.88%)

--- solutions/0015-3sum/solution.cpp
+++ solutions/0015-3sum/solution.cpp
@@ -1,55 +1,37 @@
 class Solution {
 public:
-    vector<vector<int>> res;
-
-    // Function to find pairs in the sorted array that sum up to the target
-    void sums(vector<int>& nums, int target, int i, int j) {
-        while (i < j) {
-            // If the sum of nums[i] and nums[j] is greater than the target, decrement j
-            if (nums[i] + nums[j] > target) {
-                j--;
-            }
-            // If the sum of nums[i] and nums[j] is less than the target, increment i
-            else if (nums[i] + nums[j] < target) {
-                i++;
-            }
-            // If the sum of nums[i] and nums[j] equals the target
-            else {
-                // Avoid duplicates
-                while (i < j && nums[i] == nums[i + 1]) i++;
-                while (i < j && nums[j] == nums[j - 1]) j--;
-
-                // Add the triplet to the result
-                res.push_back({-target, nums[i], nums[j]});
-
-                // Move to the next elements
-                i++;
-                j--;
-            }
-        }
-    }
-
-    // Function to find all unique triplets in the array which gives the sum of zero
     vector<vector<int>> threeSum(vector<int>& nums) {
-        // Sort the array
+        vector<vector<int>> res;
+
+        // Sort the array to use two pointers technique
         sort(nums.begin(), nums.end());
 
-        // Iterate through the array
-        for (int i = 0; i < nums.size(); i++) {
-            // Skip duplicate elements
-            if (i > 0 && nums[i] == nums[i - 1]) {
-                continue;
+        // Traverse the array
+        for (int i = 0; i < nums.size(); ++i) {
+            // Avoid duplicates
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            int j = i + 1, k = nums.size() - 1;
+
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+
+                if (sum > 0) {
+                    --k; // Decrease k to reduce the sum
+                } else if (sum < 0) {
+                    ++j; // Increase j to increase the sum
+                } else {
+                    // Found a triplet
+                    res.push_back({nums[i], nums[j], nums[k]});
+                    // Avoid duplicates for j and k
+                    while (j < k && nums[j] == nums[j + 1]) ++j;
+                    while (j < k && nums[k] == nums[k - 1]) --k;
+                    ++j;
+                    --k;
+                }
             }
-
-            // Define the first element of the triplet and the target for the pair sum
-            int n1 = nums[i];
-            int target = -n1;
-
-            // Find pairs that sum up to the target
-            sums(nums, target, i + 1, nums.size() - 1);
         }
 
-        // Return the result
         return res;
     }
 };
