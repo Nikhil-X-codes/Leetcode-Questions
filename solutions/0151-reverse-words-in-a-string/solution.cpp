410346eab2a380cc96509a3dadea19d171c08969
NIKHIL NAGAR <nagarn2005@gmail.com>
2024-09-09 16:30:44 +0000

    [LeetCode Sync] Runtime - 3 ms (34.06%), Memory - 8.7 MB (99.98%)

--- solutions/0151-reverse-words-in-a-string/solution.cpp
+++ solutions/0151-reverse-words-in-a-string/solution.cpp
@@ -1,35 +1,31 @@
 class Solution {
 public:
     string reverseWords(string s) {
+    
+    reverse(s.begin(),s.end());
 
-        reverse(s.begin(), s.end());
-        
-        int i = 0;
-        int l = 0, r = 0;
-        
-        int n = s.length();
-        
-        while(i < n) {
-            while(i < n && s[i] != ' ') {
-                s[r++] = s[i++];
-            }
-            
-            if(l < r) { //l     r
-                reverse(s.begin()+l, s.begin()+r);
-                
-                s[r] = ' ';
-                r++;
-                
-                l = r;
-            }
-            
-            i++; //y eto badhta rahega
+    int n=s.size();
+    int l=0,r=0;
+    int i=0;
+
+    while(i < n){
+
+        while(i < n and s[i] != ' '){
+         s[r++]=s[i++];
         }
-        
-        s = s.substr(0 , r-1);
-        
-        return s;
-        
+
+        if(l < r){
+         reverse(s.begin()+l, s.begin()+r);
+
+            s[r]=' ';
+            r++;
+
+            l=r;
+        }
+i++;
+    }
+s=s.substr(0,r-1);
+return s;
     }
 };
 
