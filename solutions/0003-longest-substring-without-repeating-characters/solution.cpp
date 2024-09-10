902264ec85025ea7b880617e2131ef4ad7d3a662
NIKHIL NAGAR <nagarn2005@gmail.com>
2024-09-10 15:52:23 +0000

    [LeetCode Sync] Runtime - 10 ms (52.09%), Memory - 11.9 MB (48.56%)

--- solutions/0003-longest-substring-without-repeating-characters/solution.cpp
+++ solutions/0003-longest-substring-without-repeating-characters/solution.cpp
@@ -2,22 +2,23 @@ class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
         
+        unordered_map<char,int>mp;
+
         int st=0,end=0;
         int maxlen=0;
 
-        unordered_map<char,int>mp;
-
-        while(end < s.size()){
-
-            if(mp.find(s[end]) != mp.end()){
-                st = max(st, mp[s[end]] + 1);
-            }
+        while(end < s.length()){
+         
+    if(mp.find(s[end]) != mp.end()){
+       st = max(st, mp[s[end]] + 1);
+    }
             
 mp[s[end]] = end;
- maxlen = max(maxlen, end - st + 1);
 
-            end++;
+maxlen=max(maxlen,end-st+1);
+
+      end++;
         }
-        return maxlen;
+return maxlen;
     }
 };
