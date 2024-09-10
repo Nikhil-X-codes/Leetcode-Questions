b151775cd46ad053ffed6f6f2ff6ef82b89f9881
NIKHIL NAGAR <nagarn2005@gmail.com>
2024-09-10 15:34:37 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 7.7 MB (100.00%)

--- solutions/0028-find-the-index-of-the-first-occurrence-in-a-string/solution.cpp
+++ solutions/0028-find-the-index-of-the-first-occurrence-in-a-string/solution.cpp
@@ -5,22 +5,22 @@ public:
         int m = haystack.size();
         int n = needle.size();
 
-        if (n == 0) return 0; // If needle is empty, return 0
-        if (m < n) return -1; // If haystack is smaller than needle, return -1
+        if (n == 0) return 0;
+        if (m < n) return -1; 
 
         for(int i = 0; i < m; i++) {
 
             for(int j = 0; j < n; j++) {
 
                 if(haystack[i + j] != needle[j]) {
-                    break; // If characters don't match, break out of the inner loop
+                    break; 
                 }
 
                 if(j == n - 1)
-                    return i; // If we have reached the end of the needle, return the starting index
+                    return i;
             }
         }
-        return -1; // If no match is found, return -1
+        return -1;
     }
 };
 
