ce75da5c9aabf330b10ed368c667e3d21a211d9c
NIKHIL NAGAR <nagarn2005@gmail.com>
2024-10-07 13:18:13 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 8.2 MB (55.67%)

--- solutions/0202-happy-number/solution.cpp
+++ solutions/0202-happy-number/solution.cpp
@@ -1,26 +1,29 @@
 class Solution {
 public:
     bool isHappy(int n) {
+
+        unordered_set<int>mp;
+
+        while(true){
+            int sum=0;
+
+            while(n!=0){
+             int digit=n%10;
+             sum+=digit*digit;
+             n/=10;
+            }
         
-       unordered_set<int>s;
 
-       while(n !=1 and s.find(n) == s.end()){
+        if(sum==1) return true;
 
-        s.insert(n);
-        int sum=0;
-
-        while(n > 0){
-            int digit=n%10;
-            sum+=digit*digit;
-            n/=10;
+        if(mp.find(sum) != mp.end()){
+            return false;
         }
+        
+        mp.insert(sum);
         n=sum;
-       }
 
-       if(n==1){
-        return true;
-       }
+        }
 
-return false;
     }
 };
