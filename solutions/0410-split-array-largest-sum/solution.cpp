01bc279b2038a066f7f8f6c9f3aff69fc917239f
NIKHIL NAGAR <nagarn2005@gmail.com>
2024-10-13 17:25:25 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 9.5 MB (100.00%)

---
+++ solutions/0410-split-array-largest-sum/solution.cpp
@@ -0,0 +1,54 @@
+
+class Solution {
+public:
+    bool possible(vector<int>& nums, int k, int mid) {
+        int pagesum = 0;
+        int students = 1;
+
+        for (int i = 0; i < nums.size(); i++) {
+
+            if (pagesum + nums[i] <= mid) {
+                pagesum += nums[i];
+            }
+            
+             else {
+                students++;
+                if (students > k || nums[i] > mid) return false;
+                pagesum = nums[i];
+            }
+        }
+        return true;
+    }
+
+    int splitArray(vector<int>& nums, int k) {
+
+        int sum = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+            sum += nums[i];
+        }
+
+        int st = *max_element(nums.begin(), nums.end());
+        int end = sum;
+        int ans = -1;
+
+        while (st <= end) {
+
+            int mid = st + (end - st) / 2;
+
+            if (possible(nums, k, mid)) {
+                ans = mid;
+                end = mid - 1;
+            }
+            
+             else {
+                st = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+};
+
+
+
