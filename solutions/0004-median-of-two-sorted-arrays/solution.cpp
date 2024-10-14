d1104851a64d0b92f9c940c0d3175c7c0d9fff4b
NIKHIL NAGAR <nagarn2005@gmail.com>
2024-10-14 16:31:51 +0000

    [LeetCode Sync] Runtime - 20 ms (5.79%), Memory - 94.7 MB (99.97%)

--- solutions/0004-median-of-two-sorted-arrays/solution.cpp
+++ solutions/0004-median-of-two-sorted-arrays/solution.cpp
@@ -1,42 +1,43 @@
-#include <vector>
-#include <algorithm>
-using namespace std;
-
 class Solution {
 public:
-    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        
+        
+        if (nums1.size() > nums2.size()) 
+
+            return findMedianSortedArrays(nums2, nums1);
+
         int m = nums1.size();
         int n = nums2.size();
-        int totalLength = m + n;
-        vector<int> nums3(totalLength);
-        
-        int i = 0, j = 0, k = 0;
 
-        // Merge two sorted arrays
-        while (i < m && j < n) {
-            if (nums1[i] < nums2[j]) {
-                nums3[k++] = nums1[i++];
-            } else {
-                nums3[k++] = nums2[j++];
+        int l = 0, r = m;
+
+        while (l <= r) {
+
+            int px = (l + r) / 2;
+            int py = (m + n + 1) / 2 - px;
+
+            int x1 = (px == 0) ? INT_MIN : nums1[px - 1];
+            int x2 = (py == 0) ? INT_MIN : nums2[py - 1];
+
+            int x3 = (px == m) ? INT_MAX : nums1[px];
+            int x4 = (py == n) ? INT_MAX : nums2[py];
+
+            if (x1 <= x4 && x2 <= x3) {
+
+                if ((m + n) % 2 == 0)
+                    return (max(x1, x2) + min(x3, x4)) / 2.0;
+
+                else
+                    return max(x1, x2);
             }
-        }
 
-        // Copy any remaining elements from nums1
-        while (i < m) {
-            nums3[k++] = nums1[i++];
-        }
+            if(x1 > x4) {r=px-1;}
 
-        // Copy any remaining elements from nums2
-        while (j < n) {
-            nums3[k++] = nums2[j++];
-        }
+            else  {l=px+1;}
 
-        // Calculate the median
-        if (totalLength % 2 == 1) {
-            return nums3[totalLength / 2];
-        } else {
-            return (nums3[totalLength / 2 - 1] + nums3[totalLength / 2]) / 2.0;
-        }
+             }
+
+        return -1;
     }
 };
-
