11b8bc6dc4ec6e39a7cbcda15c7767c69849691d
NIKHIL NAGAR <nagarn2005@gmail.com>
2024-10-17 15:56:43 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 8 MB (99.96%)

--- solutions/0058-length-of-last-word/solution.cpp
+++ solutions/0058-length-of-last-word/solution.cpp
@@ -2,19 +2,18 @@ class Solution {
 public:
     int lengthOfLastWord(string s) {
         
-        int len=0;
-int i=s.size()-1;
+            int len=0;
+            int i=s.size()-1;
 
-while(i>=0 and s[i] == ' '){
-i--;
-}
+             while( i>= 0 and s[i] == ' '){
+                i--;
+            }
 
-while(i>=0 and s[i] != ' '){
-len++;
-i--;
-}
+            while( i>= 0 and s[i] != ' '){
+                len++;
+                i--;
+            }
 
 return len;
-}
-
+    }
 };
