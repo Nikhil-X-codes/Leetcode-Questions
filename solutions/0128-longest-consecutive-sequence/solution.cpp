30bc3bd3a37a6a4d67590d36296580e62f735733
NIKHIL NAGAR <nagarn2005@gmail.com>
2024-11-17 15:49:19 +0000

    [LeetCode Sync] Runtime - 985 ms (5.07%), Memory - 74.8 MB (77.59%)

--- solutions/0128-longest-consecutive-sequence/solution.cpp
+++ solutions/0128-longest-consecutive-sequence/solution.cpp
@@ -2,27 +2,29 @@ class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
         
-        if (nums.empty()) return 0;
+        if(nums.size() == 0) return 0;
 
-        unordered_set<int> s(nums.begin(), nums.end());
-        int ans = 0;
+        unordered_set<int>s(nums.begin(), nums.end());
 
-        for (int i = 0; i < nums.size(); i++) {
-            if (s.find(nums[i] - 1) != s.end()) {
-                continue;
-            } else {
-                int count = 0;
-                int current = nums[i];
+        int ans=0;
 
-                while (s.find(current) != s.end()) {
-                    count++;
-                    current++;
-                }
-                ans = max(ans, count); 
+        for(int i=0;i<nums.size();i++){
+          
+          if(s.find(nums[i] -1) != s.end()) continue;
+
+          else{
+
+            int count=0;
+            int current=nums[i];
+
+            while(s.find(current) != s.end()){
+               count++;
+               current++;
             }
+           ans=max(ans,count);
+          }
         }
 
-        return ans;
+return ans;
     }
 };
-
