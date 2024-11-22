fe6688dc1d6cd578701f8eb1986574c951954231
NIKHIL NAGAR <nagarn2005@gmail.com>
2024-11-22 14:17:26 +0000

    [LeetCode Sync] Runtime - 455 ms (96.61%), Memory - 0.0B (100.00%)

---
+++ solutions/1245-user-activity-for-the-past-30-days-i/solution.sql
@@ -0,0 +1,4 @@
+# Write your MySQL query statement below
+
+select  activity_date as day ,count(distinct user_id) as active_users FROM Activity WHERE activity_date > "2019-06-27" AND activity_date <= "2019-07-27" GROUP BY activity_date ORDER BY 
+activity_date;
