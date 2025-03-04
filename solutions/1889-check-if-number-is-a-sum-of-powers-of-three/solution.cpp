5c322c94e9f1960048d37380569a531c177a8e92
NIKHIL NAGAR <nagarn2005@gmail.com>
2025-03-04 06:11:17 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 7.9 MB (34.99%)

---
+++ solutions/1889-check-if-number-is-a-sum-of-powers-of-three/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool checkPowersOfThree(int n) {
+        
+        while(n > 0){
+
+            int rem=n % 3;
+
+            if(rem == 2) return false;
+
+            n/=3;
+        }
+
+        return true;
+    }
+};
