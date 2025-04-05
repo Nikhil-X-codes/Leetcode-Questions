184694ba674bbaeac0a1e253215a84d99bd8f0fa
NIKHIL NAGAR <nagarn2005@gmail.com>
2025-04-05 15:44:58 +0000

    [LeetCode Sync] Runtime - 19 ms (7.56%), Memory - 23.4 MB (60.36%)

---
+++ solutions/1508-longest-happy-prefix/solution.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    string longestPrefix(string s) {
+        
+        int n=s.size();
+        vector<int>lps(n,0);
+
+        lps[0]=0;
+
+        int i=1;
+        int len = 0;
+        
+      while (i < n) {
+
+            if (s[i] == s[len]) {
+                len++;
+                lps[i] = len;
+                i++;
+            } 
+            
+            else {
+                if (len != 0) {
+                    len = lps[len - 1];
+                } else {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+        
+        int prefixLen = lps[n - 1];
+        return s.substr(0, prefixLen);
+    }
+};
