c407a69490e6d7ea20e6081d0a9324282732344c
NIKHIL NAGAR <nagarn2005@gmail.com>
2025-05-15 15:52:49 +0000

    [LeetCode Sync] Runtime - 46 ms (55.60%), Memory - 57.7 MB (53.27%)

---
+++ solutions/0131-palindrome-partitioning/solution.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    
+    bool isPalindrome(const string &s, int start, int end) {
+    while (start < end) {
+        if (s[start] != s[end])
+            return false;
+        start++;
+        end--;
+    }
+    return true;
+}
+
+    void solve(vector<vector<string>>&output,vector<string>&res,int index,string s){
+         
+        if(index >= s.size()){
+            output.push_back(res);
+            return;
+        }
+
+        for(int i=index;i<s.size();i++){
+
+            if(isPalindrome(s,index,i)){
+                res.push_back(s.substr(index,i-index+1));
+                solve(output,res,i+1,s);
+                res.pop_back();
+            }
+        }
+    }
+
+    vector<vector<string>> partition(string s) {
+        
+         vector<vector<string>>output;
+         vector<string>res;
+         int index=0;
+           
+         solve(output,res,index,s);
+         return output;
+    }
+};
