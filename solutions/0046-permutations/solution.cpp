a8cf5ed6e02c24268ee6bf19e2f81f6d4961f77f
NIKHIL NAGAR <nagarn2005@gmail.com>
2025-08-03 04:46:04 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 10.6 MB (51.72%)

--- solutions/0046-permutations/solution.cpp
+++ solutions/0046-permutations/solution.cpp
@@ -1,26 +1,32 @@
 class Solution {
 public:
    
-   void solve( vector<vector<int>>&res,vector<int>& nums,int index){
-   
-     if(index >= nums.size()){
+    void solve( vector<vector<int>>&res,vector<int>& nums,int index){
+      
+      if(index >= nums.size()){
         res.push_back(nums);
         return;
-     }
+      }
 
-     for(int i=index;i<nums.size();i++){
-            swap(nums[index], nums[i]);      
-            solve(res, nums, index + 1);        
-            swap(nums[index], nums[i]);  
-     }
-   }
-   
+      for(int i=index;i<nums.size();i++){
+        swap(nums[index],nums[i]);
+        solve(res,nums,index+1);
+
+        // backtrack
+        swap(nums[index],nums[i]);
+      }
+
+    }
+
+
     vector<vector<int>> permute(vector<int>& nums) {
         
-         vector<vector<int>>res;
-         int index=0;
-         solve(res,nums,index);
+          vector<vector<int>>res;
 
-         return res;
+          int index=0;
+         
+          solve(res,nums,index);
+          return res;
+
     }
 };
