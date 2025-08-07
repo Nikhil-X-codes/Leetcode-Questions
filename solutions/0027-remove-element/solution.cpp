35bca7a367fc44f7044f0966a4d49acf9d7474a6
NIKHIL NAGAR <nagarn2005@gmail.com>
2025-08-07 13:28:36 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 11.8 MB (13.00%)

--- solutions/0027-remove-element/solution.cpp
+++ solutions/0027-remove-element/solution.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-
-    int slow = 0;
-    for (int fast = 0; fast < nums.size(); ++fast) {
-        if (nums[fast] != val) {
-            nums[slow] = nums[fast];
-            ++slow;
+        int i=0;
+        for(int j=0;j<nums.size();j++){
+            if(nums[j] != val){
+              nums[i]=nums[j];
+              i++;
+            }
         }
+        return i;
     }
-    return slow;
-   }
 };
