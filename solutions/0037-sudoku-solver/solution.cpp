bddc0d94265a80b2ce3b13ea94658288dd315c22
NIKHIL NAGAR <nagarn2005@gmail.com>
2025-08-08 14:11:48 +0000

    [LeetCode Sync] Runtime - 247 ms (74.31%), Memory - 8.7 MB (94.80%)

---
+++ solutions/0037-sudoku-solver/solution.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+
+    bool isValid(vector<vector<char>>& board, int row, int col, char num) {
+        // Check row
+        for (int j = 0; j < 9; j++) {
+            if (board[row][j] == num) return false;
+        }
+        
+        // Check column
+        for (int i = 0; i < 9; i++) {
+            if (board[i][col] == num) return false;
+        }
+        
+        // Check 3x3 box
+        int startRow = 3 * (row / 3);
+        int startCol = 3 * (col / 3);
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[startRow + i][startCol + j] == num) return false;
+            }
+        }
+        
+        return true;
+    }
+    
+    bool solve(vector<vector<char>>& board) {
+        
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] == '.') {
+                    for (char val = '1'; val <= '9'; val++) {
+                        if (isValid(board, i, j, val)) {
+                            board[i][j] = val;
+                            
+                            if (solve(board)) {
+                                return true;
+                            }
+                            
+                            board[i][j] = '.'; // backtrack
+                        }
+                    }
+                    return false; // No valid number found for this cell
+                }
+            }
+        }
+        return true; // All cells filled successfully
+    }
+    
+    void solveSudoku(vector<vector<char>>& board) {
+        solve(board);
+    }
+};
