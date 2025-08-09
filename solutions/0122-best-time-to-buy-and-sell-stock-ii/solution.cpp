e2f1843ee7fa53f645005a640a1e4c02a5726a8a
NIKHIL NAGAR <nagarn2005@gmail.com>
2025-08-09 05:30:40 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 16.9 MB (100.00%)

--- solutions/0122-best-time-to-buy-and-sell-stock-ii/solution.cpp
+++ solutions/0122-best-time-to-buy-and-sell-stock-ii/solution.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        
+        int n=prices.size();
         int profit=0;
 
-        for(int i=1;i<prices.size();i++){
-
-if(prices[i] > prices[i-1]){
-profit+=(prices[i] - prices[i-1]);
-}
+        for(int i=0;i<n-1;i++){
+            if(prices[i] < prices[i+1]){
+              profit+=prices[i+1] - prices[i];
+            }
         }
-return profit;    }
+     return profit;
+    }
 };
