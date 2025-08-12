ccbc0e25727d0fbd089c45750df1837b79dc1ffb
NIKHIL NAGAR <nagarn2005@gmail.com>
2025-08-12 12:54:11 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 22.8 MB (68.22%)

--- solutions/0135-candy/solution.cpp
+++ solutions/0135-candy/solution.cpp
@@ -1,32 +1,27 @@
 class Solution {
 public:
     int candy(vector<int>& ratings) {
-        int n = ratings.size();
-        if (n == 0) return 0;
+        int n=ratings.size();
+        vector<int>res(n,1);
 
-        vector<int> candies(n, 1);
-
-        // First pass: left to right
-        for (int i = 1; i < n; i++) {
-            if (ratings[i] > ratings[i-1]) {
-                candies[i] = candies[i-1] + 1;
+        for(int i=1;i<n;i++){
+            if(ratings[i]> ratings[i-1]){
+                res[i] = res[i-1] + 1; 
             }
         }
 
-        // Second pass: right to left
-        for (int i = n - 2; i >= 0; i--) {
-            if (ratings[i] > ratings[i+1]) {
-                candies[i] = max(candies[i], candies[i+1] + 1);
+        for(int i=n-2;i>=0;i--){
+            if(ratings[i]> ratings[i+1]){
+                res[i] = max(res[i], res[i+1] + 1);
             }
         }
 
-        // Calculate the total number of candies
         int total = 0;
         for (int i = 0; i < n; i++) {
-            total += candies[i];
+            total += res[i];
         }
 
         return total;
+
     }
 };
-
