eaa197a50a4b08606e210afe21e770632259fea0
NIKHIL NAGAR <nagarn2005@gmail.com>
2025-08-15 08:26:42 +0000

    [LeetCode Sync] Runtime - 14 ms (5.44%), Memory - 16.6 MB (5.12%)

--- solutions/0012-integer-to-roman/solution.cpp
+++ solutions/0012-integer-to-roman/solution.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
+    map<int,string> mp = {
+        {1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"},
+        {100, "C"}, {90, "XC"}, {50, "L"}, {40, "XL"},
+        {10, "X"}, {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"}
+    };
+    
     string intToRoman(int num) {
-        
-        map<int, string, greater<int>> roman = {
-            {1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"},
-            {100, "C"}, {90, "XC"}, {50, "L"}, {40, "XL"},
-            {10, "X"}, {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"}
-        };
-
-        string res = "";
-
-        for (auto it = roman.begin(); it != roman.end(); ++it) {
+        string s = "";
+        for (auto it = mp.rbegin(); it != mp.rend(); it++) {
             while (num >= it->first) {
+                s += it->second;
                 num -= it->first;
-                res += it->second;
             }
         }
-
-        return res;
+        return s;
     }
 };
