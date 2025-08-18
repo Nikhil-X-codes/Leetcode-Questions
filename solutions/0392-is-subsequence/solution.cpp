3d08d5433471eef0a8439ff971a61fe31edb2994
NIKHIL NAGAR <nagarn2005@gmail.com>
2025-08-18 10:44:41 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 8.5 MB (67.86%)

--- solutions/0392-is-subsequence/solution.cpp
+++ solutions/0392-is-subsequence/solution.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
-        int i = 0, j = 0;
 
-        while (i < s.length() && j < t.length()) {
-            if (s[i] == t[j]) {
+        if(s.size() > t.size()) return false;
+        int i=0,j=0;
+
+        while(j < t.size()){
+            if(s[i] == t[j]){
                 i++;
             }
+
             j++;
         }
 
-        return (i == s.length()) ? true : false;
+        return (i == s.size()) ? true : false;
     }
 };
-
