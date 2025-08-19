b2c2c965a61aa5ad6da93b08ca53217976fe4cab
NIKHIL NAGAR <nagarn2005@gmail.com>
2025-08-19 12:15:51 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 8.8 MB (99.64%)

--- solutions/0028-find-the-index-of-the-first-occurrence-in-a-string/solution.cpp
+++ solutions/0028-find-the-index-of-the-first-occurrence-in-a-string/solution.cpp
@@ -1,17 +1,50 @@
 class Solution {
 public:
-    int strStr(string haystack, string needle) {
+    vector<int> lpsarray(string needle) {
+        int m = needle.size();
+        vector<int> lps(m, 0);
 
-        int m = haystack.size();
-        int n = needle.size();
+        int len = 0;
+        int i = 1;
 
-        if (n == 0) return 0; 
-        if (m < n) return -1; 
-
-        for(int i=0;i<=m-n;i++){
-          if(haystack.substr(i,n) == needle) return i;
+        while (i < m) {
+            if (needle[i] == needle[len]) {
+                len++;
+                lps[i] = len;
+                i++;
+            } else {
+                if (len != 0) {
+                    len = lps[len - 1]; // ✅ correct way
+                } else {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
         }
+        return lps;
+    }
 
+    int strStr(string haystack, string needle) {
+        if (needle.empty()) return 0;
+
+        vector<int> lps = lpsarray(needle);
+
+        int i = 0, j = 0; // i -> haystack, j -> needle
+        while (i < haystack.size()) {
+            if (haystack[i] == needle[j]) {
+                i++; j++;
+            }
+
+            if (j == needle.size()) {
+                return i - j; // match found
+            } else if (i < haystack.size() && haystack[i] != needle[j]) {
+                if (j != 0) {
+                    j = lps[j - 1]; // use prefix info
+                } else {
+                    i++;
+                }
+            }
+        }
         return -1;
     }
 };
