a3c5ce27f5ae4fdeece7c5d62dd571a4689209d0
NIKHIL NAGAR <nagarn2005@gmail.com>
2025-08-20 15:37:23 +0000

    [LeetCode Sync] Runtime - 10 ms (98.94%), Memory - 17.6 MB (95.94%)

---
+++ solutions/0236-lowest-common-ancestor-of-a-binary-tree/solution.cpp
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        
+        if(root == NULL || root == p || root == q) return root;
+
+        TreeNode* lefty=lowestCommonAncestor(root->left,p,q);
+        TreeNode* righty=lowestCommonAncestor(root->right,p,q);
+
+         if(lefty && righty) return root; 
+
+         return lefty ? lefty : righty;   // ek null hai aur dusra non null hai
+
+    }
+};
