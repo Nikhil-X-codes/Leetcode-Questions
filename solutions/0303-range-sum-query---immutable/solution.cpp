c552b9c61bd6045a98c3c29619e4b587537ee34c
NIKHIL NAGAR <nagarn2005@gmail.com>
2025-08-28 16:24:31 +0000

    [LeetCode Sync] Runtime - 1 ms (57.93%), Memory - 23.9 MB (62.96%)

--- solutions/0303-range-sum-query---immutable/solution.cpp
+++ solutions/0303-range-sum-query---immutable/solution.cpp
@@ -1,14 +1,18 @@
 class NumArray {
 public:
-    vector<int>nums;
-    
+vector<int> prefix;
+
     NumArray(vector<int>& nums) {
-        this->nums=nums;
+        int n=nums.size();
+         prefix.resize(n + 1, 0);
+
+        for(int i=0;i<n;i++){
+           prefix[i+1]=prefix[i]+nums[i];
+        }
     }
     
     int sumRange(int left, int right) {
-        
-        return accumulate(nums.begin() + left,nums.begin() + right +1,0);
+        return prefix[right+1]-prefix[left];
     }
 };
 
