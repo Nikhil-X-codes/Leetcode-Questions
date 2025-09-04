39bb6a90beed08113117a610cd57fee301a4e38b
NIKHIL NAGAR <nagarn2005@gmail.com>
2025-09-04 12:21:23 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 8.9 MB (13.04%)

--- solutions/0290-word-pattern/solution.cpp
+++ solutions/0290-word-pattern/solution.cpp
@@ -1,46 +1,43 @@
+#include <iostream>
+#include <sstream>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
 class Solution {
 public:
     bool wordPattern(string pattern, string s) {
-        
-    vector<string> words;
-    stringstream ss(s);
-    string word;
-    
-    while (ss >> word) {
-        words.push_back(word);
-    }
 
-    unordered_map<char, string> mp1;  
-    unordered_map<string, char> mp2;
-
-    if(words.size() != pattern.size()) return false;
-
-    for(int i=0;i<pattern.size();i++){
-
-        char c1=pattern[i];
-        string s1=words[i];
-
-        if(mp1.count(c1)){
-           
-           if(mp1[c1] != s1) return false;
+        vector<string> arr;
+        stringstream ss(s);
+        string word;
+        while (ss >> word) {
+            arr.push_back(word);
         }
 
-        else{
-            mp1[c1]=s1;
+        if (pattern.size() != arr.size()) return false;
+
+        unordered_map<char, string> mp;    
+        unordered_map<string, char> mp1;   
+
+        for (int i = 0; i < pattern.size(); i++) {
+            char c = pattern[i];
+            string w = arr[i];
+
+            if (mp.find(c) != mp.end()) {
+                if (mp[c] != w) return false; 
+            } else {
+                mp[c] = w;
+            }
+
+            if (mp1.find(w) != mp1.end()) {
+                if (mp1[w] != c) return false; 
+            } else {
+                mp1[w] = c;
+            }
         }
 
-        
-        if(mp2.count(s1)){
-           
-           if(mp2[s1] != c1) return false;
-        }
-
-        else{
-            mp2[s1]=c1;
-        }
-
-    }
-
-    return true;
+        return true;
     }
 };
+
