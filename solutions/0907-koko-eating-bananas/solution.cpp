879803f941ea13dd9de6ff0f9f8be7238ee44933
NIKHIL NAGAR <nagarn2005@gmail.com>
2025-09-07 10:54:05 +0000

    [LeetCode Sync] Runtime - 10 ms (39.61%), Memory - 22.9 MB (53.58%)

--- solutions/0907-koko-eating-bananas/solution.cpp
+++ solutions/0907-koko-eating-bananas/solution.cpp
@@ -1,30 +1,28 @@
 class Solution {
 public:
-
-    bool istrue(vector<int>& piles, int mid, int h) {
-
-        int actualhours = 0;
-
-        for (int x : piles) {
-            actualhours += x / mid;
-            if (x % mid != 0) actualhours++;
+    
+    bool issolve(vector<int>& piles, int h, int mid) {
+        long long hours = 0;  
+        for (int bananas : piles) {
+            hours += (bananas + mid - 1) / mid;
+            if (hours > h) return false;    
         }
-
-        return actualhours <= h;
+        return hours <= h;
     }
 
     int minEatingSpeed(vector<int>& piles, int h) {
-        
         int l = 1;
         int r = *max_element(piles.begin(), piles.end());
 
         while (l < r) {
             int mid = l + (r - l) / 2;
-            if (istrue(piles, mid, h)) r = mid;
-            else l = mid + 1;
+            if (issolve(piles, h, mid)) {
+                r = mid;   
+            } else {
+                l = mid + 1; 
+            }
         }
-
-        return l;
+        return l; 
     }
 };
 
