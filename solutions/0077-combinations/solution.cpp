22e998cb105842b61ba1c52cc0c6055b126aee64
NIKHIL NAGAR <nagarn2005@gmail.com>
2025-09-08 17:15:27 +0000

    [LeetCode Sync] Runtime - 70 ms (52.69%), Memory - 110.1 MB (20.56%)

--- solutions/0077-combinations/solution.cpp
+++ solutions/0077-combinations/solution.cpp
@@ -1,28 +1,27 @@
 class Solution {
 public:
-   
-   void solve(vector<vector<int>>&output,vector<int>&res,int index,int n,int k){
-     
-    if(res.size() == k){
-       output.push_back(res);
-       return;
-     }
+    void solve(vector<vector<int>> &output, vector<int> &res, int n, int k, int i) {
 
-     for(int i=index;i<=n;i++){
-       res.push_back(i);
-       solve(output,res,i+1,n,k);
-       res.pop_back();
-     }
+        if (res.size() == k) {
+            output.push_back(res);
+            return;
+        }
 
-   }
-   
+        if (i > n) return;
+
+        res.push_back(i);
+        solve(output, res, n, k, i + 1);
+
+
+        res.pop_back(); 
+        solve(output, res, n, k, i + 1);
+    }
+
     vector<vector<int>> combine(int n, int k) {
-        
-        vector<vector<int>>output;
-        vector<int>res;
-        int index=1;
-
-        solve(output,res,index,n,k);
+        vector<vector<int>> output;
+        vector<int> res;
+        solve(output, res, n, k, 1);
         return output;
     }
 };
+
