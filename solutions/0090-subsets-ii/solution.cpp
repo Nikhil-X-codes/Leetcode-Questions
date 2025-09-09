5293f625921da943a6173083c67ab1e52dd5b90c
NIKHIL NAGAR <nagarn2005@gmail.com>
2025-09-09 14:35:22 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 10.4 MB (68.33%)

--- solutions/0090-subsets-ii/solution.cpp
+++ solutions/0090-subsets-ii/solution.cpp
@@ -1,34 +1,33 @@
 class Solution {
 public:
 
-   void solve(vector<vector<int>>&output,vector<int>&res,vector<int>nums,int index,int n){
+    void solve(vector<int>& nums, vector<vector<int>>& output, vector<int>& res, int index){
+         
+        output.push_back(res);
 
-    output.push_back(res);
-    
-    for(int i=index;i<n;i++){
+        for(int i=index;i<nums.size();i++){
+          
+          if(i > index && nums[i] == nums[i-1]) continue;
 
-        if(i > index && nums[i] == nums[i-1]) continue;
+          res.push_back(nums[i]);
+          solve(nums,output,res,i+1);
+
+          res.pop_back();
+        }
+
+          
         
-        res.push_back(nums[i]);
-        solve(output,res,nums,i+1,n);
-
-        // backtrack
-        res.pop_back();
-
     }
-
-   }
-
 
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         
-         vector<vector<int>>output;
-         vector<int>res;
-         int index=0;
-         int n=nums.size();
-         
-         sort(nums.begin(),nums.end());
-         solve(output,res,nums,index,n);
-         return output;
+        sort(nums.begin(),nums.end());
+
+        vector<vector<int>>output;
+        vector<int>res;
+
+        solve(nums,output,res,0);
+
+        return output;
     }
 };
