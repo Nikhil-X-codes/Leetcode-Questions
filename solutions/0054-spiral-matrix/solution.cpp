4d51a0159969974c963bdbd3ef60bbcaae86f5d8
NIKHIL NAGAR <nagarn2005@gmail.com>
2025-09-09 17:25:28 +0000

    [LeetCode Sync] Runtime - 2 ms (1.80%), Memory - 9.3 MB (84.17%)

--- solutions/0054-spiral-matrix/solution.cpp
+++ solutions/0054-spiral-matrix/solution.cpp
@@ -1,39 +1,34 @@
-#include <vector>
-
 class Solution {
 public:
-    std::vector<int> spiralOrder(std::vector<std::vector<int>>& matrix) {
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        if (matrix.empty()) return {};
         int m = matrix.size();
         int n = matrix[0].size();
 
-        std::vector<int> res;
+        vector<int> res;
+        int st_row = 0, end_row = m - 1;
+        int st_col = 0, end_col = n - 1;
 
-        int st_row = 0, last_row = m - 1;
-        int st_col = 0, last_col = n - 1;
+        while (st_row <= end_row && st_col <= end_col) {
 
-        while (st_row <= last_row && st_col <= last_col) {
-            for (int i = st_col; i <= last_col; i++) {
-                res.push_back(matrix[st_row][i]);
-            }
-            st_row++;
+            for (int j = st_col; j <= end_col; ++j)
+                res.push_back(matrix[st_row][j]);
+            ++st_row;
 
-            for (int i = st_row; i <= last_row; i++) {
-                res.push_back(matrix[i][last_col]);
-            }
-            last_col--;
+            for (int i = st_row; i <= end_row; ++i)
+                res.push_back(matrix[i][end_col]);
+            --end_col;
 
-            if (st_row <= last_row) {
-                for (int i = last_col; i >= st_col; i--) {
-                    res.push_back(matrix[last_row][i]);
-                }
-                last_row--;
+            if (st_row <= end_row) {
+                for (int j = end_col; j >= st_col; --j)
+                    res.push_back(matrix[end_row][j]);
+                --end_row;
             }
 
-            if (st_col <= last_col) {
-                for (int i = last_row; i >= st_row; i--) {
-                    res.push_back(matrix[i][st_col]);
-                }
-                st_col++;
+            if (st_col <= end_col) {
+                for (int i = end_row; i >= st_row; --i)
+                    res.push_back(matrix[i][st_col]); 
+                ++st_col;
             }
         }
 
