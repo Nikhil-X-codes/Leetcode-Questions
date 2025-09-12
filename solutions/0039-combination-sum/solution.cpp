d7cd197ec8fd2df1f0ab35ed7088861227590a2a
NIKHIL NAGAR <nagarn2005@gmail.com>
2025-09-12 09:14:43 +0000

    [LeetCode Sync] Runtime - 7 ms (24.81%), Memory - 23.1 MB (14.39%)

--- solutions/0039-combination-sum/solution.cpp
+++ solutions/0039-combination-sum/solution.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
     void solve(vector<vector<int>>&output,vector<int>&res,vector<int>candidates,int target,int index){
+
         if(target == 0){
             output.push_back(res);
             return;
         }
         
-        // Use OR (||) and check bounds FIRST
         if(index >= candidates.size() || candidates[index] > target) 
             return;
         
-        // include
         res.push_back(candidates[index]);
         solve(output,res,candidates,target-candidates[index],index);
-        
-        // exclude and backtrack 
+
         res.pop_back();
         solve(output,res,candidates,target,index+1);
     }
