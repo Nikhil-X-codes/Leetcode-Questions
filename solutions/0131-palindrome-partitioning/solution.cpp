35ad353a7d489f6a32da17e31f02e55a49dcbf3d
NIKHIL NAGAR <nagarn2005@gmail.com>
2025-09-16 19:25:24 +0000

    [LeetCode Sync] Runtime - 32 ms (80.08%), Memory - 53.4 MB (93.05%)

--- solutions/0131-palindrome-partitioning/solution.cpp
+++ solutions/0131-palindrome-partitioning/solution.cpp
@@ -1,40 +1,36 @@
 class Solution {
 public:
     
-    bool isPalindrome(const string &s, int start, int end) {
-    while (start < end) {
-        if (s[start] != s[end])
-            return false;
-        start++;
-        end--;
+    bool isPalindrome(string &s, int start, int end) {
+    while(start < end) {
+        if(s[start] != s[end]) return false;
+        start++; end--;
     }
     return true;
 }
 
-    void solve(vector<vector<string>>&output,vector<string>&res,int index,string s){
-         
-        if(index >= s.size()){
+    void solve(vector<vector<string>>&output,vector<string>&res,int index,string &s){
+          
+        if(index == s.size()){
             output.push_back(res);
             return;
         }
 
         for(int i=index;i<s.size();i++){
-
             if(isPalindrome(s,index,i)){
-                res.push_back(s.substr(index,i-index+1));
-                solve(output,res,i+1,s);
+                res.push_back(s.substr(index, i - index + 1));
+                solve(output,res,i + 1,s);
                 res.pop_back();
             }
         }
+        
     }
 
     vector<vector<string>> partition(string s) {
-        
-         vector<vector<string>>output;
-         vector<string>res;
-         int index=0;
-           
-         solve(output,res,index,s);
-         return output;
+        vector<vector<string>>output;
+        vector<string>res;
+          
+        solve(output,res,0,s);
+        return output;
     }
 };
