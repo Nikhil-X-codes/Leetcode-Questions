4b08b52efbbee4ec4ad8f440c8fefd53cea8cd4a
NIKHIL NAGAR <nagarn2005@gmail.com>
2025-11-04 11:58:57 +0000

    [LeetCode Sync] Runtime - 67 ms (92.22%), Memory - 131.4 MB (51.26%)

--- solutions/0502-ipo/solution.cpp
+++ solutions/0502-ipo/solution.cpp
@@ -1,29 +1,33 @@
 class Solution {
 public:
-int findMaximizedCapital(int k, int w, vector<int>& profits, vector<int>& capital) {
-    int n = profits.size();
-    vector<pair<int, int>> projects(n);
-    for (int i = 0; i < n; i++) {
-        projects[i] = {capital[i], profits[i]};
-    }
-    sort(projects.begin(), projects.end());
-
-    int i = 0;
-    priority_queue<int> maximizedcapital;
-    while (k--) {
-        while (i < n && projects[i].first <= w) {
-            maximizedcapital.push(projects[i].second);
-            i++;
+    int findMaximizedCapital(int k, int w, vector<int>& profits, vector<int>& capital) {
+        
+        vector<pair<int, int>> projects;
+        int n=profits.size();
+        
+        for (int i = 0; i < n; i++) {
+            projects.push_back({capital[i], profits[i]});
         }
 
-        if (!maximizedcapital.empty()) {
-            w += maximizedcapital.top();
-            maximizedcapital.pop();
-        } 
+        sort(projects.begin(), projects.end());
+
+        priority_queue<int> maxProfitHeap;
+
+        int i = 0; 
+        while (k--) {
+
+            while (i < n && projects[i].first <= w) {
+                maxProfitHeap.push(projects[i].second);
+                i++;
+            }
+
+            if (maxProfitHeap.empty())
+                break;
+
+            w += maxProfitHeap.top();
+            maxProfitHeap.pop();
+        }
+
+        return w;
     }
-
-    return w;
-}
-
-
 };
