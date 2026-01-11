f66508dd766ee4f168c281e0fcb44dc5078f9df9
NIKHIL NAGAR <nagarn2005@gmail.com>
2026-01-11 14:41:36 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 22.2 MB (97.47%)

---
+++ solutions/0875-longest-mountain-in-array/solution.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int longestMountain(vector<int>& arr) {
+        
+        int maxlen=0;
+        int n=arr.size();;
+
+        int i=1;
+        while(i < n-1){
+           
+           int left=i-1;
+           int right=i+1;
+
+           if(arr[i] > arr[i-1] && arr[i] > arr[i+1]){
+             
+             while(left > 0 && arr[left] > arr[left-1] ){
+                left--;
+             }
+
+             while(right < n-1 && arr[right] > arr[right+1]){
+                right++;
+             }
+
+             maxlen=max(maxlen,right-left+1);
+             i=right;
+           }
+
+           else{
+            i++;
+           }
+
+        }
+
+        return maxlen;
+    }
+};
