a4a8db26eee4b38a3cf3fc961a3342a94f4bf779
NIKHIL NAGAR <nagarn2005@gmail.com>
2026-01-15 12:02:57 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 32 MB (99.61%)

--- solutions/0209-minimum-size-subarray-sum/solution.cpp
+++ solutions/0209-minimum-size-subarray-sum/solution.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
     int minSubArrayLen(int target, vector<int>& nums) {
-        
         int minlen=INT_MAX;
-        int l=0;
-        int currsum=0;
+        int left=0;
+        int sum=0;
 
-        for(int r=0;r<nums.size();r++){
+        for(int right=0;right<nums.size();right++){
+            sum+=nums[right];
 
-            currsum+=nums[r];
+            while(sum >= target){
+                minlen=min(minlen,right-left+1);
 
-            while(currsum >= target){
-                minlen=min(minlen,r-l+1);
-                currsum-=nums[l];
-                l++;
-            }
+                sum-=nums[left];
+                left++;
+                }
         }
 
-        return minlen == INT_MAX ? 0 : minlen;
+        return (minlen == INT_MAX) ? 0 : minlen;
     }
 };
