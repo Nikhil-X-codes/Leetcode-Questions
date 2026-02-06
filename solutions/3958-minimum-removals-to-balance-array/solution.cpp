4ed33b9ddf98f9e512ae0eae7c65494765912cf2
NIKHIL NAGAR <nagarn2005@gmail.com>
2026-02-06 06:04:26 +0000

    [LeetCode Sync] Runtime - 26 ms (86.10%), Memory - 105.2 MB (17.74%)

---
+++ solutions/3958-minimum-removals-to-balance-array/solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int minRemoval(vector<int>& nums, int k) {
+         
+         sort(nums.begin(),nums.end());
+         int i=0;
+         int n=nums.size();
+         int len=0;
+
+         for(int j=0;j<n;j++){
+            
+            while(nums[j] > (long long) k * nums[i]){
+               i++;
+            }
+
+            len=max(len,j-i+1);
+         }
+
+         return n - len;
+    }
+};
