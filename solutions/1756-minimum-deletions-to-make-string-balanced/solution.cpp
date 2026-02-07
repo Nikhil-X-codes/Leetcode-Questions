2e008bfcf79e1e0a8dc7b8eada0e819a3c3a87a5
NIKHIL NAGAR <nagarn2005@gmail.com>
2026-02-07 06:48:47 +0000

    [LeetCode Sync] Runtime - 41 ms (24.20%), Memory - 55.1 MB (15.60%)

---
+++ solutions/1756-minimum-deletions-to-make-string-balanced/solution.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minimumDeletions(string s) {
+        
+        int n=s.size();
+        vector<int>prefix(n+1,0);
+        vector<int>suffix(n+1,0);
+
+        for(int i=1;i<=n;i++){
+           prefix[i] = prefix[i-1] + (s[i-1] == 'b');
+        }
+
+         for(int i=n-1;i>=0;i--){
+           suffix[i] = suffix[i+1] + (s[i+1] == 'a');
+        }
+         
+        int ans=INT_MAX;
+        for(int i=0;i<=n;i++){
+            ans=min(ans,prefix[i]+suffix[i]);
+        }
+        
+        return ans;
+    }
+};
