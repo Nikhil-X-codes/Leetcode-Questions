2840e6c885d60a7c8bd82c4a9dfdb75f323a5bca
NIKHIL NAGAR <nagarn2005@gmail.com>
2026-03-18 17:28:42 +0000

    [LeetCode Sync] Runtime - 3 ms (9.48%), Memory - 9.3 MB (54.71%)

--- solutions/0062-unique-paths/solution.cpp
+++ solutions/0062-unique-paths/solution.cpp
@@ -1,30 +1,37 @@
 class Solution {
 public:
-  
+    // int solve(int m, int n, int i, int j){
+
+    //    if(i == m-1 && j == n-1) return 1;
+
+    //    if(i >= m || j >= n) return 0;
+
+    //    int right = solve(m, n, i, j+1);
+    //    int down  = solve(m, n, i+1, j);
+
+    //    return right + down;
+    // }
+
     int uniquePaths(int m, int n) {
-        vector<vector<int>> memo(m, vector<int>(n, -1));
-        return solve(0, 0, m, n, memo);
-    }
+        // return solve(m, n, 0, 0);
+        
+        if(m == 1 && n==1) return 1;
 
-    int solve(int i, int j, int m, int n, vector<vector<int>>& memo) {
+        vector<vector<int>> dp(m , vector<int>(n , 0));
 
-        if (i == m - 1 && j == n - 1) {
-            return 1;
+        for (int i = 1; i < n; i++)
+            dp[0][i] = 1;
+
+        for (int i = 1; i < m; i++)
+            dp[i][0] = 1;
+
+        for (int i = 1; i < m; i++) {
+
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
         }
 
-        if (i >= m || j >= n) {
-            return 0;
-        }
-
-        if (memo[i][j] != -1) {
-            return memo[i][j];
-        }
-
-        int right = solve(i, j + 1, m, n, memo);
-        int down = solve(i + 1, j, m, n, memo);
-
-        memo[i][j] = right + down;
-        return memo[i][j];
+        return dp[m-1][n-1];
     }
 };
-
