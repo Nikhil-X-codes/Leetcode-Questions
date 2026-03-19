ced8711fb1dde951bbabcd1259f18ab08dfd4132
NIKHIL NAGAR <nagarn2005@gmail.com>
2026-03-19 05:05:05 +0000

    [LeetCode Sync] Runtime - 50 ms (30.24%), Memory - 84.8 MB (84.48%)

--- solutions/0940-fruit-into-baskets/solution.cpp
+++ solutions/0940-fruit-into-baskets/solution.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
-
-        int maxcount = 0;
-        int l = 0;
+        
+        int l=0;
         unordered_map<int,int>mp;
+        int maxlen = 0;
 
-        for (int r = 0; r < fruits.size(); r++) {
+        for(int r=0;r<fruits.size();r++){
+            
             mp[fruits[r]]++;
 
-            while (mp.size() > 2) {
+            while(mp.size() > 2){
                 mp[fruits[l]]--;
 
-                if (mp[fruits[l]] == 0) {
-                    mp.erase(fruits[l]);
-                }
+                if(mp[fruits[l]] == 0) mp.erase(fruits[l]);
+
                 l++;
             }
-            maxcount = max(maxcount, r - l + 1);
+
+            maxlen = max(maxlen,r-l+1);
         }
 
-        return maxcount;
+        return maxlen;
     }
 };
