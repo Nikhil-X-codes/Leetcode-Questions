723bc31bb02a74951b37a90aa704997b0e55e195
NIKHIL NAGAR <nagarn2005@gmail.com>
2026-03-23 10:12:03 +0000

    [LeetCode Sync] Runtime - 2 ms (58.36%), Memory - 7.8 MB (95.66%)

--- solutions/0278-first-bad-version/solution.cpp
+++ solutions/0278-first-bad-version/solution.cpp
@@ -6,16 +6,18 @@ public:
     int firstBadVersion(int n) {
         
         int left=1,right=n;
-        while(left <= right){
+        while(left < right){
           int mid=left+(right-left)/2;
+
           if(isBadVersion(mid)){
-right = mid - 1;
+right = mid;
           }
           else{
  left = mid + 1;
           }
+
         }
 
-        return left;
+        return right;
     }
 };
