765330fcf5e4e23d673845816bd1eea80ac3513c
NIKHIL NAGAR <nagarn2005@gmail.com>
2026-03-24 13:13:53 +0000

    [LeetCode Sync] Runtime - 24 ms (20.02%), Memory - 14.8 MB (20.76%)

--- solutions/0438-find-all-anagrams-in-a-string/solution.cpp
+++ solutions/0438-find-all-anagrams-in-a-string/solution.cpp
@@ -1,43 +1,37 @@
 class Solution {
 public:
     vector<int> findAnagrams(string s, string p) {
-        
-        vector<int>res;
 
-        int m=s.size();
-        int n=p.size();
+        vector<int> res;
+        unordered_map<char, int> mp1, mp2;
+        int l = 0;
 
-        if(n > m) return {};
-
-        unordered_map<char,int>mp1,mp2;
-
-        for(char c:p) mp2[c]++;
-
-        int i=0,j=0;
-        int winsize=n;
-
-        while(j < m){
-           
-           mp1[s[j]]++;
-
-           if(j-i+1 > winsize){
-
-               mp1[s[i]]--;
-               
-               if(mp1[s[i]] == 0){
-                mp1.erase(s[i]);
-               }
-
-               i++;
-           }
-
-           if(j-i+1 == winsize && mp1 == mp2 ){
-             res.push_back(i);
-           }
-
-           j++;
+        for (char c : p) {
+            mp1[c]++;
         }
 
-return res;
+        int window = p.size();
+
+        for (int r = 0; r < s.size(); r++) {
+
+            mp2[s[r]]++;
+
+            if (r - l + 1 > window) {
+
+                mp2[s[l]]--;
+
+                if (mp2[s[l]] == 0) {
+                    mp2.erase(s[l]);
+                }
+                l++;
+            }
+
+            if (r - l + 1 == window && mp1 == mp2) {
+                res.push_back(l);
+            }
+            
+        }
+
+        return res;
     }
 };
