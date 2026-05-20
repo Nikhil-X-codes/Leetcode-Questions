06265041edd300172383ae8e88adc5f2b0d3836f
NIKHIL NAGAR <nagarn2005@gmail.com>
2026-05-20 08:23:09 +0000

    [LeetCode Sync] Runtime - 43 ms (43.73%), Memory - 53 MB (11.05%)

--- solutions/0322-coin-change/solution.cpp
+++ solutions/0322-coin-change/solution.cpp
@@ -1,21 +1,30 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-        vector<int> dp(amount + 1, INT_MAX);
+        
+        int n = coins.size();
 
-        dp[0] = 0; 
+        vector<vector<int>> dp(n + 1,
+                               vector<int>(amount + 1, 1e9));
 
-        for (int i = 1; i <= amount; i++) {
-            for (int j = 0; j < coins.size(); j++) {
-                if (i - coins[j] >= 0 && dp[i - coins[j]] != INT_MAX) {
-                    dp[i] = min(dp[i], dp[i - coins[j]] + 1);
-                }
+       for(int i=0;i<=n;i++){
+        dp[i][0] = 0;
+       }
+
+       for(int i=1;i<=n;i++){
+         
+         for(int j=1;j<=amount;j++){
+
+            if(coins[i-1] <= j){
+dp[i][j] = min(dp[i-1][j],1+dp[i][j-coins[i-1]]);
             }
-        }
 
-        if (dp[amount] == INT_MAX)
-            return -1;
-        return dp[amount];
+            else{
+dp[i][j] = dp[i-1][j];
+            }
+         }
+       }
+
+       return dp[n][amount] == 1e9 ? -1 : dp[n][amount];
     }
 };
-
