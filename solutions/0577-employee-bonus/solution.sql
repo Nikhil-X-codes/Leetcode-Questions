4107fcb882d5e72f3ec2c54aa39f8cd9bea3971b
NIKHIL NAGAR <nagarn2005@gmail.com>
2026-05-21 09:38:38 +0000

    [LeetCode Sync] Runtime - 1412 ms (8.61%), Memory - 0.0B (100.00%)

--- solutions/0577-employee-bonus/solution.sql
+++ solutions/0577-employee-bonus/solution.sql
@@ -1,5 +1,8 @@
 # Write your MySQL query statement below
 
-select e.name,b.bonus from Employee e
-left join Bonus b on e.empId = b.empId WHERE bonus < 1000 OR Bonus IS NULL;
+select e.name,b.bonus
+from Employee e
+left join Bonus b
+on e.empId = b.empId
+where b.bonus < 1000 OR b.bonus is NULL
 
