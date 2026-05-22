decc1c0aa82729c11d119cfd8c1cce6de0cd8bfe
NIKHIL NAGAR <nagarn2005@gmail.com>
2026-05-22 08:38:45 +0000

    [LeetCode Sync] Runtime - 387 ms (48.66%), Memory - 0.0B (100.00%)

--- solutions/1338-queries-quality-and-percentage/solution.sql
+++ solutions/1338-queries-quality-and-percentage/solution.sql
@@ -1,13 +1,9 @@
-SELECT
-    query_name,
-    
-    ROUND(AVG(rating / position), 2) AS quality,
-    
-    ROUND(
-        100 * SUM(CASE WHEN rating < 3 THEN 1 ELSE 0 END) / COUNT(*),
+
+select query_name,
+Round(sum(rating/position) / count(*),2) as quality,
+   ROUND(
+        SUM(CASE WHEN rating < 3 THEN 1 ELSE 0 END) * 100 / COUNT(*),
         2
-    ) AS poor_query_percentage
-
-FROM Queries
-
-GROUP BY query_name;
+    )  as poor_query_percentage
+from Queries
+group by query_name
