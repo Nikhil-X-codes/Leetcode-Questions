2909b34f9ec339b4f395b54ca6f64ba32498e539
NIKHIL NAGAR <nagarn2005@gmail.com>
2026-06-05 05:46:19 +0000

    [LeetCode Sync] Runtime - 83 ms (26.54%), Memory - 105.4 MB (58.72%)

--- solutions/2000-minimum-speed-to-arrive-on-time/solution.cpp
+++ solutions/2000-minimum-speed-to-arrive-on-time/solution.cpp
@@ -2,27 +2,25 @@ class Solution {
 public:
     bool solve(vector<int>& dist, double hour, int mid) {
 
-        double time = 0.0;
+        int n=dist.size();
 
-        for (int i = 0; i < dist.size() - 1; i++) {
+        double time=0;
 
-            time += ceil((double)dist[i] / mid);
+        for(int i=0;i<n-1;i++){
+time += ceil((double)dist[i] / mid);
         }
 
-        time += (double)dist[dist.size() - 1] / mid;
+        time += (double)dist[n-1]/mid;
 
         return time <= hour;
     }
 
     int minSpeedOnTime(vector<int>& dist, double hour) {
-        
-        if(dist.size() > ceil(hour)) return -1;
 
-        int ans = -1;
         int l = 1, r = 1e7;
+        int ans = -1;
 
         while (l <= r) {
-
             int mid = l + (r - l) / 2;
 
             if (solve(dist, hour, mid)) {
