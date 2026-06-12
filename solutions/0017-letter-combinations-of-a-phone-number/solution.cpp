8632ee19e58aab5d90be3e4b6ddf9be650a4c778
NIKHIL NAGAR <nagarn2005@gmail.com>
2026-06-12 10:11:22 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 9.4 MB (31.70%)

--- solutions/0017-letter-combinations-of-a-phone-number/solution.cpp
+++ solutions/0017-letter-combinations-of-a-phone-number/solution.cpp
@@ -1,46 +1,32 @@
 class Solution {
 public:
-    
-    unordered_map<char,string>mp={
-        {'2', "abc"},
-        {'3', "def"},
-        {'4', "ghi"},
-        {'5', "jkl"},
-        {'6', "mno"},
-        {'7', "pqrs"},
-        {'8', "tuv"},
-        {'9', "wxyz"}
-    };
+    unordered_map<char, string> mp = {{'2', "abc"}, {'3', "def"}, {'4', "ghi"},
+                                      {'5', "jkl"}, {'6', "mno"}, {'7', "pqrs"},
+                                      {'8', "tuv"}, {'9', "wxyz"}};
 
-    void solve(vector<string>&res,string &ans,string digits,int index){
+    void solve(string digits, vector<string>& res, string ans, int index) {
 
-        if(index >= digits.size()){
-           res.push_back(ans);
-           return;
+        if (index >= digits.size()) {
+            res.push_back(ans);
+            return;
         }
 
-        char val=digits[index];
-        string got=mp[val];
+        char val = digits[index];
+        string curr = mp[val];
 
-        for(int i=0;i<got.size();i++){
-            
-            ans.push_back(got[i]);
-            solve(res,ans,digits,index+1);
-
+        for (int i = 0; i < curr.size(); i++) {
+            ans.push_back(curr[i]);
+            solve(digits, res, ans, index + 1);
             ans.pop_back();
         }
-
     }
 
     vector<string> letterCombinations(string digits) {
-        
-        vector<string>res;
 
-        if(digits.empty()) return res;
+        vector<string> res;
+        string ans = "";
+        solve(digits, res, ans, 0);
 
-        string ans="";
-
-        solve(res,ans,digits,0);
         return res;
     }
 };
