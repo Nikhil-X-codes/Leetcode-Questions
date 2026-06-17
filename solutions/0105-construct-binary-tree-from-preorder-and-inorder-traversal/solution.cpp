5f9655d1074be211874702fd921627d2fad2a421
NIKHIL NAGAR <nagarn2005@gmail.com>
2026-06-17 04:05:17 +0000

    [LeetCode Sync] Runtime - 2 ms (77.68%), Memory - 27.4 MB (54.82%)

--- solutions/0105-construct-binary-tree-from-preorder-and-inorder-traversal/solution.cpp
+++ solutions/0105-construct-binary-tree-from-preorder-and-inorder-traversal/solution.cpp
@@ -1,30 +1,46 @@
-
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
 class Solution {
 public:
-    unordered_map<int, int> mp; 
+    unordered_map<int, int> mp;
 
-    TreeNode* solve(int start, int end, vector<int>& preorder, vector<int>& inorder, int& index) {
-        if (start > end) return nullptr;
+    TreeNode* solve(vector<int>& preorder, vector<int>& inorder, int& index,int st, int end) {
+
+        if (st > end)
+            return nullptr;
 
         int val = preorder[index++];
+
         TreeNode* root = new TreeNode(val);
 
         int pos = mp[val];
 
-        root->left = solve(start, pos - 1, preorder, inorder, index);
-        root->right = solve(pos + 1, end, preorder, inorder, index);
+        root->left = solve(preorder, inorder, index, st, pos - 1);
+        root->right = solve(preorder, inorder, index, pos + 1, end);
 
         return root;
     }
 
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        
-        for (int i = 0; i < inorder.size(); i++) {
+
+        int n = inorder.size();
+
+        for (int i = 0; i < n; i++) {
             mp[inorder[i]] = i;
         }
 
-        int index = 0; 
-        return solve(0, inorder.size() - 1, preorder, inorder, index);
+        int index = 0;
+
+        return solve(preorder, inorder, index, 0, n-1);
     }
 };
-
