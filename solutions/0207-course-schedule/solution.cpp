6d00ba89596120683d0ab8a9a0d8476521cf575a
NIKHIL NAGAR <nagarn2005@gmail.com>
2026-06-23 04:31:00 +0000

    [LeetCode Sync] Runtime - 1 ms (83.19%), Memory - 19.1 MB (90.59%)

--- solutions/0207-course-schedule/solution.cpp
+++ solutions/0207-course-schedule/solution.cpp
@@ -1,45 +1,42 @@
 class Solution {
 public:
-
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        
-         vector<vector<int>> adj(numCourses);
-         vector<int> indegree(numCourses, 0);
 
-         for(auto &pre:prerequisites){
-            int u=pre[0];
-            int v=pre[1];
+        vector<vector<int>> adj(numCourses);
+        vector<int> indegree(numCourses, 0);
+        queue<int> q;
+
+        for (auto& p : prerequisites) {
+            int u = p[0];
+            int v = p[1];
 
             adj[u].push_back(v);
             indegree[v]++;
-         }
-        
-        queue<int> q;
+        }
 
-        for(int i = 0; i < numCourses; i++) {
-            if(indegree[i] == 0) {
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0) {
                 q.push(i);
             }
         }
-       
+
         int count = 0;
 
-        while(!q.empty()){
-           
-           int node = q.front();
-           q.pop();
-           count++;
+        while (!q.empty()) {
 
-           for(int i:adj[node]){
-            indegree[i]--;
+            int node = q.front();
+            q.pop();
+            count++;
 
-            if(indegree[i] == 0){
-                q.push(i);
+            for (int& neigh : adj[node]) {
+                indegree[neigh]--;
+
+                if (indegree[neigh] == 0) {
+                    q.push(neigh);
+                }
             }
-
-           }
         }
-        
+
         return count == numCourses;
     }
 };
