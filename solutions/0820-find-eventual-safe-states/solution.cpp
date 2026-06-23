715041b801547e6e834ddad5a325ee455cdfab00
NIKHIL NAGAR <nagarn2005@gmail.com>
2026-06-23 05:27:40 +0000

    [LeetCode Sync] Runtime - 51 ms (40.49%), Memory - 66.2 MB (47.91%)

--- solutions/0820-find-eventual-safe-states/solution.cpp
+++ solutions/0820-find-eventual-safe-states/solution.cpp
@@ -3,46 +3,43 @@ public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
 
         vector<int> res;
-
         int n = graph.size();
 
-        vector<vector<int>> revGraph(n);
-        vector<int> outdegree(n, 0);
+        vector<vector<int>> rev(n);
+        vector<int> outdegree(n);
 
-        for (int u = 0; u < n; u++) {
-            outdegree[u] = graph[u].size();
+            for (int i = 0; i < n; i++) {
 
-            for (int v : graph[u]) {
-                revGraph[v].push_back(u);
+            outdegree[i] = graph[i].size();
+
+            for (int v : graph[i]) {
+                rev[v].push_back(i);
             }
         }
 
         queue<int> q;
 
         for (int i = 0; i < n; i++) {
-            if (outdegree[i] == 0) {
+            if (outdegree[i] == 0)
                 q.push(i);
-            }
         }
 
         while (!q.empty()) {
-
             int node = q.front();
             q.pop();
 
             res.push_back(node);
 
-            for (int i : revGraph[node]) {
-                outdegree[i]--;
+            for (int neigh : rev[node]) {
+                outdegree[neigh]--;
 
-                if (outdegree[i] == 0) {
-                    q.push(i);
+                if (outdegree[neigh] == 0) {
+                    q.push(neigh);
                 }
             }
         }
-       
-        sort(res.begin(),res.end());
 
+        sort(res.begin(), res.end());
         return res;
     }
 };
