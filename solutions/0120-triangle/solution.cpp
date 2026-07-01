80701e22c4c8d6afa8e4fc0ea8323d9e27baa72b
NIKHIL NAGAR <nagarn2005@gmail.com>
2026-07-01 02:09:39 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 13.4 MB (45.49%)

--- solutions/0120-triangle/solution.cpp
+++ solutions/0120-triangle/solution.cpp
@@ -1,25 +1,21 @@
 class Solution {
 public:
-    // int solve(vector<vector<int>>& triangle,int m,int n,int i,int j){
+    // int solve(vector<vector<int>>& triangle, int m, int i, int j) {
 
-    //     if(i == m-1) return triangle[i][j];
-    //     if(i < 0 || i >= m || j >=m || j < 0) return INT_MAX;
+    //     if (i == m - 1)
+    //         return triangle[i][j];
 
-    //     int down = solve(triangle,m,n,i+1,j);
-    //     int right = solve(triangle,m,n,i+1,j+1);
+    //     int same = solve(triangle, m, i + 1, j);
+    //     int next = solve(triangle, m, i + 1, j + 1);
 
-    //     return triangle[i][j] + min(down,right);
+    //     return triangle[i][j] + min(same, next);
     // }
 
     int minimumTotal(vector<vector<int>>& triangle) {
 
         int m = triangle.size();
-        int n = triangle[0].size();
 
-        if (m == 1 && n == 1)
-            return triangle[0][0];
-
-        //  return solve(triangle,m,n,0,0);
+        // return solve(triangle, m, 0, 0);
 
         vector<vector<int>> dp(m, vector<int>(m, 0));
 
@@ -28,11 +24,9 @@ public:
         }
 
         for (int i = m - 2; i >= 0; i--) {
-            for (int j = 0; j <= i; j++) {
-                int down = dp[i + 1][j];
-                int right = dp[i + 1][j + 1];
 
-                dp[i][j] = triangle[i][j] + min(down, right);
+            for (int j = 0; j <= i; j++) {
+                dp[i][j] = triangle[i][j] + min(dp[i + 1][j], dp[i + 1][j + 1]);
             }
         }
 
