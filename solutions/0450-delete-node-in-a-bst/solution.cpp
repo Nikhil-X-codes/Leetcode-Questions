23c1932cc0e24047d5f70e7f7a7fba89aae27833
NIKHIL NAGAR <nagarn2005@gmail.com>
2026-07-14 02:44:16 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 34.3 MB (52.47%)

--- solutions/0450-delete-node-in-a-bst/solution.cpp
+++ solutions/0450-delete-node-in-a-bst/solution.cpp
@@ -1,41 +1,69 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
 class Solution {
 public:
-    TreeNode* succ(TreeNode* root) {
-        while (root->left)
-            root = root->left;
+
+  TreeNode* predecessor(TreeNode* root){
+        
+        root = root->left;
+
+        while(root->right){
+            root = root->right;
+        }
+
         return root;
-    }
+  }
 
     TreeNode* deleteNode(TreeNode* root, int key) {
 
         if (root == NULL)
             return NULL;
 
-        if (key < root->val) {
+        if (root->val > key) {
             root->left = deleteNode(root->left, key);
         }
-        else if (key > root->val) {
+
+        else if (root->val < key) {
             root->right = deleteNode(root->right, key);
         }
-        else { // node found
 
-            if (root->left == NULL) {
+        else {
+
+            if (root->left == NULL && root->right == NULL) {
+                delete root;
+                return NULL;
+            }
+
+            else if (root->left == NULL) {
                 TreeNode* temp = root->right;
                 delete root;
                 return temp;
             }
 
-            if (root->right == NULL) {
+            else if (root->right == NULL) {
                 TreeNode* temp = root->left;
                 delete root;
                 return temp;
             }
 
-            TreeNode* successor = succ(root->right);
+            else {
 
-            root->val = successor->val;
+                TreeNode* prev = predecessor(root);
 
-            root->right = deleteNode(root->right, successor->val);
+                root->val = prev->val;
+
+                root->left = deleteNode(root->left,prev->val);
+            }
         }
 
         return root;
