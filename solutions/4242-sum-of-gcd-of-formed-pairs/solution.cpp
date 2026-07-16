7810f967f723c12e88fbc912fe99aa0554c932ac
NIKHIL NAGAR <nagarn2005@gmail.com>
2026-07-16 08:39:00 +0000

    [LeetCode Sync] Runtime - 71 ms (65.24%), Memory - 155.6 MB (73.24%)

--- solutions/4242-sum-of-gcd-of-formed-pairs/solution.cpp
+++ solutions/4242-sum-of-gcd-of-formed-pairs/solution.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     long long gcdSum(vector<int>& nums) {
+        if (nums.empty()) return 0;
 
         int n = nums.size();
+
         vector<int> prefixGcd(n);
 
         int maxi = nums[0];
 
-        for(int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) {
             maxi = max(maxi, nums[i]);
             prefixGcd[i] = gcd(nums[i], maxi);
         }
@@ -16,8 +18,8 @@ public:
 
         long long sum = 0;
 
-        for(int i = 0; i < n/2; i++) {
-            sum += gcd(prefixGcd[i], prefixGcd[n-i-1]);
+        for (int i = 0; i < n / 2; i++) {
+            sum += gcd(prefixGcd[i], prefixGcd[n - i - 1]);
         }
 
         return sum;
