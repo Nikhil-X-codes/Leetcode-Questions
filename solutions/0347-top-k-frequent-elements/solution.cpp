edfc8afcaf86f30fdcf6b8b3a7387ac2e31ea9be
NIKHIL NAGAR <nagarn2005@gmail.com>
2026-07-16 13:05:54 +0000

    [LeetCode Sync] Runtime - 2 ms (57.89%), Memory - 18 MB (42.13%)

--- solutions/0347-top-k-frequent-elements/solution.cpp
+++ solutions/0347-top-k-frequent-elements/solution.cpp
@@ -1,27 +1,31 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
+        
+        unordered_map<int,int> mp;
+
+        for(int num : nums){
+            mp[num]++;
+        }
+
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> minheap;
+
+        for(auto &element : mp){
+            int num = element.first;
+            int freq = element.second;
+
+            minheap.push({freq, num});
+
+            if(minheap.size() > k){
+                minheap.pop();
+            }
+        }
 
         vector<int> res;
 
-        unordered_map<int, int> mp;
-
-        for (int i : nums) {
-            mp[i]++;
-        }
-
-        multimap<int, int, greater<int>> MM;
-
-        for (auto& i : mp) {
-            MM.insert({i.second, i.first});
-        }
-
-        for (auto& i : MM) {
-
-            if (res.size() < k) {
-                res.push_back(i.second);
-            }
-
+        while(!minheap.empty()){
+            res.push_back(minheap.top().second);
+            minheap.pop();
         }
 
         return res;
