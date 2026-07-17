cc71e664d16ada645aac2b1a7885359281246008
NIKHIL NAGAR <nagarn2005@gmail.com>
2026-07-17 07:54:47 +0000

    [LeetCode Sync] Runtime - 43 ms (21.54%), Memory - 47.7 MB (31.88%)

--- solutions/0621-task-scheduler/solution.cpp
+++ solutions/0621-task-scheduler/solution.cpp
@@ -1,49 +1,51 @@
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
-        
-        unordered_map<int,int>mp;
-        for(char c:tasks){
+
+        int count = 0;
+
+        unordered_map<char, int> mp;
+
+        for (char c : tasks) {
             mp[c]++;
         }
 
-        priority_queue<int>q;
-        for(auto m:mp){
-            q.push(m.second);
+        priority_queue<int> q;
+
+        for (auto i : mp) {
+            q.push({i.second});
         }
 
-        int count=0;
+        while (!q.empty()) {
 
-       while(!q.empty()){
-           
-        vector<int>res;
+            vector<int> res;
 
-        for(int i=1;i<=n+1;i++){
-         
-         if(!q.empty()){
-            int freq=q.top();
-            q.pop();
-            freq--;
-            res.push_back(freq);
-         }
+            for (int i = 1; i <= n + 1; i++) {
 
-        }
-
-        for(int i:res){
-            if(i > 0){
-                q.push(i);
+                if (!q.empty()) {
+                    int mostfreq = q.top();
+                    q.pop();
+                    mostfreq--;
+                    res.push_back(mostfreq);
+                }
             }
+
+            for (int i : res) {
+                if (i > 0) {
+                    q.push(i);
+                }
+            }
+
+            if (q.empty()) {
+                count += res.size();
+            }
+
+            else {
+                count += n + 1;
+            }
+            
         }
 
-        if(q.empty()){
-          count+=res.size();
-        }
-
-        else{
-          count+=n+1;
-        }
-
-       }
         return count;
     }
 };
