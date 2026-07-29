a66cb8797c5c606766fee1e99a9e16eff101dddf
NIKHIL NAGAR <nagarn2005@gmail.com>
2026-07-29 05:46:00 +0000

    [LeetCode Sync] Runtime - 2 ms (41.09%), Memory - 56.1 MB (18.70%)

--- solutions/0811-number-of-subarrays-with-bounded-maximum/solution.cpp
+++ solutions/0811-number-of-subarrays-with-bounded-maximum/solution.cpp
@@ -1,29 +1,25 @@
 class Solution {
 public:
+    int countways(vector<int>& nums, int element) {
 
-    int solve(vector<int> nums, int val){
+        int count = 0;
+        int len = 0;
 
-       int count=0;
-       int len=0;
+        for (int i = 0; i < nums.size(); i++) {
 
-       for(int i=0;i<nums.size();i++){
+            if (nums[i] <= element)
+                len++;
+            else
+                len = 0;
 
-         if(nums[i] <= val){
-           len++;
-         }
-         else{
-           len=0;
-         }
+            count += len;
+        }
 
-         count+=len;
-       }
-
-       return count;
+        return count;
     }
 
-int numSubarrayBoundedMax(vector<int>& nums, int left, int right)
-{
-    return solve(nums,right)-solve(nums,left-1);
-}
+    int numSubarrayBoundedMax(vector<int>& nums, int left, int right) {
 
+        return countways(nums, right) - countways(nums, left-1);
+    }
 };
