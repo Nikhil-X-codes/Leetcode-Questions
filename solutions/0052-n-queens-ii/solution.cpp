2cfd9a81ec07c2a35c4cc433eb235505bc339932
NIKHIL NAGAR <nagarn2005@gmail.com>
2026-08-01 06:11:22 +0000

    [LeetCode Sync] Runtime - 3 ms (39.34%), Memory - 9.2 MB (19.54%)

--- solutions/0052-n-queens-ii/solution.cpp
+++ solutions/0052-n-queens-ii/solution.cpp
@@ -1,47 +1,52 @@
-#include <bits/stdc++.h>
-using namespace std;
-
 class Solution {
 public:
+    bool isValid(vector<string>& board, int row, int col, int n) {
 
-    bool isvalid(int r, int c, vector<string>& board, int n) {
-        // check left side of row
-        for (int i = 0; i < c; i++) {
-            if (board[r][i] == 'Q') return false;
+        for (int j = 0; j < col; j++) {
+            if (board[row][j] == 'Q')
+                return false;
         }
-        // check upper-left diagonal
-        for (int i = r, j = c; i >= 0 && j >= 0; i--, j--) {
-            if (board[i][j] == 'Q') return false;
+
+        for (int i = row, j = col; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q')
+                return false;
         }
-        // check lower-left diagonal
-        for (int i = r, j = c; i < n && j >= 0; i++, j--) {
-            if (board[i][j] == 'Q') return false;
+
+        for (int i = row, j = col; i < n && j >= 0; i++, j--) {
+            if (board[i][j] == 'Q')
+                return false;
         }
+
         return true;
     }
 
-    // backtracking function
-    int solve(int c, vector<string>& board, vector<vector<string>>& res, int n) {
-        if (c == n) {
-            res.push_back(board); // store valid board config
-            return 1;             // found 1 valid solution
+    int solve(vector<vector<string>>& ans, vector<string>& board, int n,
+              int col) {
+
+        if (col == n) {
+            ans.push_back(board);
+            return 1;
         }
 
         int count = 0;
-        for (int r = 0; r < n; r++) {
-            if (isvalid(r, c, board, n)) {
-                board[r][c] = 'Q';                  // place queen
-                count += solve(c + 1, board, res, n); // recurse
-                board[r][c] = '.';                  // backtrack
+
+        for (int row = 0; row < n; row++) {
+
+            if (isValid(board, row, col, n)) {
+                board[row][col] = 'Q';
+                count += solve(ans, board, n, col + 1);
+                board[row][col] = '.';
             }
         }
+
         return count;
     }
 
     int totalNQueens(int n) {
-        vector<vector<string>> res;
-        vector<string> board(n, string(n, '.')); 
-        return solve(0, board, res, n);
+
+        vector<vector<string>> ans;
+        vector<string> board(n, string(n, '.'));
+
+        return solve(ans, board, n, 0);
     }
 };
-
