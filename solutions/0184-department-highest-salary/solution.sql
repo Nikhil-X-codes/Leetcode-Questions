b0e37ddf1beb382a6f41edcd3fa32da226deef2e
NIKHIL NAGAR <nagarn2005@gmail.com>
2026-08-31 19:19:25 +0000

    [LeetCode Sync] Runtime - 853 ms (46.68%), Memory - 0.0B (100.00%)

--- solutions/0184-department-highest-salary/solution.sql
+++ solutions/0184-department-highest-salary/solution.sql
@@ -1,15 +1,19 @@
 # Write your MySQL query statement below
 
+select d.name as Department,
+e.name as Employee,
+e.salary as Salary
+from
 
-select d.name AS Department,
-       e.name AS Employee,
-       e.salary AS Salary
-from Employee e
+(
+select * , DENSE_RANK() over(
+     PARTITION by departmentId
+    order by salary DESC
+) as rnk
+from Employee
+
+)e
+
 left join Department d
 on e.departmentId = d.id
-
-where e.salary = (
-    select MAX(salary)
-    from Employee
-    where departmentId = d.id
-)
+WHERE e.rnk = 1;
