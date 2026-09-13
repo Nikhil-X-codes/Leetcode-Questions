09bee359ef56280819d9dba299f0381901e1d87b
NIKHIL NAGAR <nagarn2005@gmail.com>
2026-09-13 17:00:41 +0000

    [LeetCode Sync] Runtime - 138 ms (28.86%), Memory - 16 MB (21.92%)

---
+++ solutions/0864-image-overlap/solution.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int largestOverlap(vector<vector<int>>& img1, vector<vector<int>>& img2) {
+        
+        int n = img1.size();
+
+        vector<pair<int, int>> ones1;
+        vector<pair<int, int>> ones2;
+
+        for (int r = 0; r < n; r++) {
+            for (int c = 0; c < n; c++) {
+                if (img1[r][c] == 1)
+                    ones1.push_back({r, c});
+
+                if (img2[r][c] == 1)
+                    ones2.push_back({r, c});
+            }
+        }
+
+        int ans = 0;
+
+        map<pair<int,int>,int>mp;
+
+        for(auto [r1,c1]:ones1){
+
+            for(auto [r2,c2]:ones2){
+                 
+                 int dr = r2-r1;
+                 int dc = c2-c1;
+
+                 mp[{dr,dc}]++;
+                 ans = max(ans,mp[{dr, dc}]);
+            }
+        }
+        
+        return ans;
+
+    }
+};
