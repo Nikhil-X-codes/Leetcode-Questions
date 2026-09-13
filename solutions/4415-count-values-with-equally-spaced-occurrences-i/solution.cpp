ba32fdd4bd30b2be8e5a9a1393e8a0f3c0636222
NIKHIL NAGAR <nagarn2005@gmail.com>
2026-09-13 04:30:38 +0000

    [LeetCode Sync] Runtime - 23 ms (10.00%), Memory - 35.1 MB (20.00%)

---
+++ solutions/4415-count-values-with-equally-spaced-occurrences-i/solution.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int countSpecialIntegers(vector<int>& nums) {
+        
+        unordered_map<int, vector<int>> mp;
+
+        for (int i = 0; i < nums.size(); i++) {
+            mp[nums[i]].push_back(i);
+        }
+
+        int count = 0;
+
+        for (auto it : mp) {
+            vector<int> v = it.second;
+
+            if (v.size() == 3) {
+                
+                if (v[1] - v[0] == v[2] - v[1]) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+};
