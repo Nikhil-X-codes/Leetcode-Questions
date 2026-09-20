564fb9135b3bb68e848e8327792a42670174f6b8
NIKHIL NAGAR <nagarn2005@gmail.com>
2026-09-20 04:23:15 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 9.8 MB (47.88%)

--- solutions/3811-reverse-degree-of-a-string/solution.cpp
+++ solutions/3811-reverse-degree-of-a-string/solution.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
+   
     int reverseDegree(string s) {
+        
+        int n = s.size();
+        int total = 0;
 
-         int n = s.size();
-        int sum = 0;
-
-        for (int i = 0; i < n; i++) {  
-            sum += (i + 1) * (26 - (s[i] - 'a'));
+        for(int i=0;i<n;i++){
+            int digit = 'z' - s[i] + 1;
+            total += (i+1) * digit;
         }
 
-        return sum;
+        return total;
     }
 };
